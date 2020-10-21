bcf104d727cd4b1bca0c955222f20fde8e47c7bc
Kata Sule <kata.sule@gmail.com>
2020-10-21 12:32:07 +0200

    add the RScript for Kata's part

---
+++ Task1_KFC-team-coding/Kata_codes.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(readxl)
+
+pizza_data <- read_excel("C:/CEU/Fall_Term/Github_Repos/homework_codesANDmore_Coding1_MScBA/Task1_KFC-team-coding/KFC-team_pizza_data.xlsx")
+
+# creating factor variables for delivery vs offline price
+pizza_data$online <- factor( pizza_data$delivery )
+table( pizza_data$online )
+pizza_data %>% select( online, price_pizza ) %>% 
+  group_by( online ) %>% 
+  summarise( mean = mean( price_pizza ),
+             sd = sd( price_pizza ),
+             num_obs = n() )
+
+# create ggplot for online vs offline prices: histogram
+ggplot( data = pizza_data, aes( x = price_pizza, fill = online ) ) +
+  geom_histogram( aes( y = ..density.. ), alpha = 0.4 ) +
+  labs( x = 'Price of Pizza', y = 'Relative frequency', fill = 'Online' )
+#  facet_wrap( ~country ) +
+#  xlim( -4, 4 )
