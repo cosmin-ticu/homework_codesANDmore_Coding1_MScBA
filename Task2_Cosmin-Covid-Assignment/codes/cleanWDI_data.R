cde7acdeb9fa42a1d5aa2d2c055f6db580be9864
Cosmin Catalin Ticu <cosmin.cata99@gmail.com>
2020-11-25 20:27:25 +0200

    finalized individual cleaning

--- Task2_Cosmin-Covid-Assignment/codes/cleanWDI_data.R
+++ Task2_Cosmin-Covid-Assignment/codes/cleanWDI_data.R
@@ -4,12 +4,12 @@ rm(list=ls())
 library(tidyverse)
 
 # Call the data from github
-df <- read.csv('https://raw.githubusercontent.com/cosmin-ticu/homework_codesANDmore_Coding1_MScBA/master/Task2_Cosmin-Covid-Assignment/data/raw/WDI_population_raw.csv')
+wd <- read.csv('https://raw.githubusercontent.com/cosmin-ticu/homework_codesANDmore_Coding1_MScBA/master/Task2_Cosmin-Covid-Assignment/data/raw/WDI_population_raw.csv')
 
 ## Check the observations:
 #   Lot of grouping observations
 #   Filter these out
-df <- df %>% filter( !grepl("[[:digit:]]", df$iso2c) )
+wd <- wd %>% filter( !grepl("[[:digit:]]", wd$iso2c) )
 
 # Some grouping observations are still there, check each of them
 #   HK - Hong Kong, China
@@ -20,30 +20,30 @@ df <- df %>% filter( !grepl("[[:digit:]]", df$iso2c) )
 # 1st drop specific values
 drop_id <- c("EU","HK","OE")
 # Check for filtering
-df %>% filter( grepl( paste( drop_id , collapse="|"), df$iso2c ) ) 
+wd %>% filter( grepl( paste( drop_id , collapse="|"), wd$iso2c ) ) 
 # Save the opposite
-df <- df %>% filter( !grepl( paste( drop_id , collapse="|"), df$iso2c ) )
+wd <- wd %>% filter( !grepl( paste( drop_id , collapse="|"), wd$iso2c ) )
 
 # 2nd drop values with certain starting char
 # Get the first letter from iso2c
-fl_iso2c <- substr(df$iso2c, 1, 1)
+fl_iso2c <- substr(wd$iso2c, 1, 1)
 retain_id <- c("XK","ZA","ZM","ZW") # one option is with code
 retain_country <- c("Kosovo","South Africa", "Zambia","Zimbabwe") # one option is with name
 # Check
-d1 <- df %>% filter( grepl( "X", fl_iso2c ) | grepl( "Z", fl_iso2c ) & 
-                       !grepl( paste( retain_country , collapse="|"), df$country )) # Kosovo gets removed regardless of method
+d1 <- wd %>% filter( grepl( "X", fl_iso2c ) | grepl( "Z", fl_iso2c ) & 
+                       !grepl( paste( retain_country , collapse="|"), wd$country )) # Kosovo gets removed regardless of method
 # Save observations which are the opposite (use of !)
-df <- df %>% filter( !( grepl( "X", fl_iso2c ) | grepl( "Z", fl_iso2c ) & 
-                          !grepl( paste( retain_country , collapse="|"), df$country ) ) )
+wd <- wd %>% filter( !( grepl( "X", fl_iso2c ) | grepl( "Z", fl_iso2c ) & 
+                          !grepl( paste( retain_country , collapse="|"), wd$country ) ) )
 
 # Clear non-needed variables
 rm( d1 , drop_id, fl_iso2c , retain_id )
 
 ### 
 # Check for missing observations
-m <- df %>% filter( !complete.cases( df ) )
+m <- wd %>% filter( !complete.cases( wd ) )
 # Drop if total population missing -> if not complete case except iso2c
-df <- df %>% filter( complete.cases( df ) | is.na( df$iso2c ) )
+wd <- wd %>% filter( complete.cases( wd ) | is.na( wd$iso2c ) )
 
 ###
 # CLEAN VARIABLES
@@ -51,13 +51,13 @@ df <- df %>% filter( complete.cases( df ) | is.na( df$iso2c ) )
 # Recreate table:
 #   Rename variables
 #   Drop all the others !! in this case write into readme it is referring to year 2019!!
-df <- df %>% transmute( country = country,
+wd <- wd %>% transmute( country = country,
                         population=SP.POP.TOTL)
 
 ###
 # Check for extreme values
 # all HISTOGRAMS
-df %>%
+wd %>%
   keep(is.numeric) %>% 
   gather() %>% 
   ggplot(aes(value)) +
@@ -65,8 +65,8 @@ df %>%
   geom_histogram()
 
 # Check for summary as well
-summary( df )
+summary( wd )
 
-# Save the raw data file
+# Save the clean data file
 my_path <- "C:/Users/cosmi/Documents/homework_codesANDmore_Coding1_MScBA/Task2_Cosmin-Covid-Assignment/data/"
-write_csv( df, paste0(my_path,'clean/WDI_population_clean.csv'))
+write_csv( wd, paste0(my_path,'clean/WDI_population_clean.csv'))
